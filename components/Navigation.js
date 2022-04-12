f62a9488e6a07104c8918e0a2cfe2d76a75d3291
chenriroo <chenriroo_dev@tutanota.com>
2022-04-12 23:28:37 +0200

    Rework navigation click outside navigation to close if open

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,30 +1,67 @@
-import { useState } from 'react'
-
+import { useState, useEffect, useRef } from 'react'
 import styles from '../styles/Navigation.module.scss'
-import PanelSlider from './PanelSlider'
+import DatePicker from './Datepicker';
+import Entries from './Entries';
 
 export default function Navigation({
-	handleSelectEntry, handleCreateEntry, inputDate, curEntries, isFetching, curEntry, curDate
+	handleSelectEntry, handleCreateEntry, inputDate, curEntries, isFetching,
+	curEntry, curDate
 }) {
-	const [isHidden, setMenu] = useState(false)
+	const [menuIsOpen, setMenuIsOpen] = useState(true)
+	const fooRef = useRef();
+	
+	useEffect(() => {
+		if(menuIsOpen) {
+			document.addEventListener('mousedown', handleClickOutside)
+		} else {
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+	}, [menuIsOpen])
+
+	function handleClickOutside(e) {
+		console.log(fooRef.current)
+		if(fooRef.current && fooRef.current.contains(e.target)) {
+			console.log('inside click')
+			return
+		}
+		setMenuIsOpen(false);
+	}
+
+	function handleClickNav() {
+		console.log('handleClickNav')
+		setMenuIsOpen(!menuIsOpen)
+	}
+
+	console.log(menuIsOpen)
 
 	return (
-		<nav className={styles.navigation}>
+		<nav 
+		className={`${styles.navigation} ${!menuIsOpen && styles.navigationCollapsed}`}
+		ref={fooRef}>
 
-			<PanelSlider
-				curEntries={curEntries}
-				inputDate={inputDate}
-				handleSelectEntry={handleSelectEntry}
-				handleCreateEntry={handleCreateEntry}
-				isFetching={isFetching}
-				isHidden={isHidden}
-				curEntry={curEntry}
-				curDate={curDate}
-			/>
-
-			<div className={styles.menubar}>
-				<button onClick={() => setMenu(!isHidden)}>Entries</button>
+			<div 
+			className={`${styles.menubar} ${!menuIsOpen && styles.menubarCollapsed}`}
+			onClick={handleClickNav}> 
 			</div>
+
+
+			<div className={styles.picker} >
+				<DatePicker inputDate={inputDate} />
+
+				<Entries 
+					curEntries={curEntries}
+					handleSelectEntry={handleSelectEntry}
+					handleCreateEntry={handleCreateEntry}
+					isFetching={isFetching}
+					curEntry={curEntry}
+					curDate={curDate}
+				/>
+			</div>
+
+
 
 		</nav>
 	)
